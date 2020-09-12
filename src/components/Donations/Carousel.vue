88dae0ddfb4e5ecb9d2d48cb8c3707f05afce0a8
Marcos Medeiros <m4arcos@gmail.com>
2020-09-12 17:45:57 -0300

    Criados componentes da home

--- src/components/Donations/Carousel.vue
+++ src/components/Donations/Carousel.vue
@@ -1,7 +1,8 @@
 <template>
   <v-sheet class="mx-auto" elevation="8" max-width="85%">
-    <v-slide-group v-model="model" class="pa-4" show-arrows>
-      <v-slide-item v-for="n in 15" :key="n" v-slot:default="{ active, toggle }">
+    <h4 class="headline text-left carousel-title">{{ title }}</h4>
+    <v-slide-group v-model="model" class="pa-4 carousel-group" show-arrows>
+      <v-slide-item v-for="item in items" :key="item.id" v-slot:default="{ active, toggle }">
         <v-card
           :color="active ? 'light-green' : 'light-green lighten-1'"
           class="ma-4"
@@ -9,11 +10,11 @@
           width="300"
           @click="toggle"
         >
-          <v-card-title class="headline font-weight-bold">Unlimited music now</v-card-title>
+          <v-card-title class="headline font-weight-bold">{{ item.title }}</v-card-title>
 
           <v-card-text
             class="text-left"
-          >"Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."</v-card-text>
+          >{{ item.summary }}</v-card-text>
           <v-card-actions>
             <v-btn class="font-weight-bold" text>Saiba mais...</v-btn>
           </v-card-actions>
@@ -29,7 +30,7 @@
     <v-expand-transition>
       <v-sheet v-if="model != null" color="grey lighten-4" height="200" tile>
         <v-row class="fill-height" align="center" justify="center">
-          <h3 class="title">Selected {{ model }}</h3>
+          <h3 class="title carousel-text">Selected {{ items[model].text }}</h3>
         </v-row>
       </v-sheet>
     </v-expand-transition>
@@ -38,6 +39,11 @@
 
 <script>
 export default {
+  name: "donations-carousel",
+  props: {
+    title: String,
+    items: {}
+  },
   data: () => ({
     model: null,
   }),
@@ -45,7 +51,16 @@ export default {
 </script>
 
 <style scoped>
-.headline {
-  /* color: #ddd !important; */
+.carousel-title {
+  padding-left: 10px;
+  padding-top: 10px;
+}
+
+.carousel-text {
+  padding: 25px;
+}
+
+.carousel-group {
+  padding-top: 5px !important;
 }
 </style>
--- src/components/Home/Index.vue
+++ src/components/Home/Index.vue
@@ -2,7 +2,16 @@
   <v-container fluid>
     <v-row align="center" justify="center">
       <v-col cols="12">
-        <donations-carousel></donations-carousel>
+        <donations-carousel
+        :title="donations.title"
+        :items="donations.items" />
+      </v-col>
+    </v-row>
+    <v-row align="center" justify="center">
+      <v-col cols="12">
+        <donations-carousel
+        :title="needsHelp.title"
+        :items="needsHelp.items" />
       </v-col>
     </v-row>
   </v-container>
@@ -18,6 +27,56 @@ export default {
   },
   data() {
     return {
+      donations: {
+        title: "Para doação",
+        items: [
+          {
+            title: "Título 1",
+            summary: "Estou disponível para doação",
+            text: "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it?"
+          },
+          {
+            title: "Título 2",
+            summary: "Estou disponível para doação",
+            text: "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it?"
+          },
+          {
+            title: "Título 3",
+            summary: "Estou disponível para doação",
+            text: "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it?"
+          },
+          {
+            title: "Título 4",
+            summary: "Estou disponível para doação",
+            text: "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it?"
+          },
+        ]
+      },
+      needsHelp: {
+        title: "Preciso de ajuda",
+        items: [
+          {
+            title: "Preciso de ajuda 1",
+            summary: "Estou precisando de ajuda",
+            text: "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it?"
+          },
+          {
+            title: "Preciso de ajuda 2",
+            summary: "Estou precisando de ajuda",
+            text: "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it?"
+          },
+          {
+            title: "Preciso de ajuda 3",
+            summary: "Estou precisando de ajuda",
+            text: "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it?"
+          },
+          {
+            title: "Preciso de ajuda 4",
+            summary: "Estou precisando de ajuda",
+            text: "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it?"
+          },
+        ]
+      },
     };
   },
   mounted() {
