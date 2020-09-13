965265ef10371f072036e9dcdda588633db28c42
Marcos Medeiros <m4arcos@gmail.com>
2020-09-13 16:41:47 -0300

    Correção para titulos das páginas

--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,57 @@ import OngsIndex from './components/Ongs/Index.vue'
 import NeedsIndex from './components/Needs/Index.vue'
 import UsersIndex from './components/Users/Index.vue'
 
+const siteTitle = "Ajude.me";
+
 const routes = [
-    { path: '/', component: HomeIndex, name: 'home' },
-    { path: '/login', component: LoginIndex, name: 'login' },
-    { path: '/donations', component: DonationsIndex, name: 'donations' },
-    { path: '/ongs', component: OngsIndex, name: 'ongs' },
-    { path: '/needs', component: NeedsIndex, name: 'needs' },
-    { path: '/users', component: UsersIndex, name: 'users' },
+    {
+        path: '/',
+        component: HomeIndex,
+        name: 'home',
+        meta: {
+            title: siteTitle + ""
+        }
+    },
+    {
+        path: '/login',
+        component: LoginIndex,
+        name: 'login',
+        meta: {
+            title: siteTitle + " - Login"
+        }
+    },
+    {
+        path: '/donations',
+        component: DonationsIndex,
+        name: 'donations',
+        meta: {
+            title: siteTitle + " - Quero Ajudar!"
+        }
+    },
+    {
+        path: '/ongs',
+        component: OngsIndex,
+        name: 'ongs',
+        meta: {
+            title: siteTitle + " - ONGs"
+        }
+    },
+    {
+        path: '/needs',
+        component: NeedsIndex,
+        name: 'needs',
+        meta: {
+            title: siteTitle + " - Preciso de ajuda!"
+        }
+    },
+    {
+        path: '/users',
+        component: UsersIndex,
+        name: 'users',
+        meta: {
+            title: siteTitle + " - Usuários"
+        }
+    },
 ];
 
 export default routes;
