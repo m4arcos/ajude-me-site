06d367e85270623b5f5c4527b781aefa3c105ac8
Marcos Medeiros <marcos.medeiros@appmax.org>
2020-09-14 09:54:23 -0300

    Adicionado loader na home

--- src/components/DonationsCarousel.vue
+++ src/components/DonationsCarousel.vue
@@ -1,40 +1,54 @@
 <template>
   <v-sheet class="mx-auto" elevation="8" max-width="85%">
+    <v-progress-linear v-if="loading" indeterminate color="primary"></v-progress-linear>
     <h4 class="headline text-left carousel-title">
       <v-icon>{{icon}}</v-icon>
       {{ title }}
     </h4>
-    <v-slide-group v-model="model" class="pa-4 carousel-group" show-arrows>
-      <v-slide-item v-for="item in items" :key="item.id" v-slot:default="{ active, toggle }">
-        <v-card
-          :color="active ? 'cardsSelectedBG' : 'cardsBG'"
-          class="ma-4"
-          height="200"
-          width="300"
-          @click="toggle"
-        >
-          <v-card-title class="headline font-weight-bold">{{ item.title }}</v-card-title>
+    <template v-if="!loading && items.length > 0">
+      <v-slide-group v-model="model" class="pa-4 carousel-group" show-arrows>
+        <v-slide-item v-for="item in items" :key="item.id" v-slot:default="{ active, toggle }">
+          <v-card
+            :color="active ? 'cardsSelectedBG' : 'cardsBG'"
+            class="ma-4"
+            height="200"
+            width="300"
+            @click="toggle"
+          >
+            <v-card-title class="headline font-weight-bold">{{ item.title }}</v-card-title>
 
-          <v-card-text class="text-left">{{ item.summary }}</v-card-text>
-          <v-card-actions>
-            <v-btn class="font-weight-bold" text>Saiba mais...</v-btn>
-          </v-card-actions>
+            <v-card-text class="text-left">{{ item.summary }}</v-card-text>
+            <v-card-actions>
+              <v-btn class="font-weight-bold" text>Saiba mais...</v-btn>
+            </v-card-actions>
+            <v-row class="fill-height" align="center" justify="center">
+              <v-scale-transition>
+                <v-icon v-if="active" color="white" size="48" v-text="'mdi-close-circle-outline'"></v-icon>
+              </v-scale-transition>
+            </v-row>
+          </v-card>
+        </v-slide-item>
+      </v-slide-group>
+
+      <v-expand-transition>
+        <v-sheet v-if="model != null" color="grey lighten-4" height="200" tile>
           <v-row class="fill-height" align="center" justify="center">
-            <v-scale-transition>
-              <v-icon v-if="active" color="white" size="48" v-text="'mdi-close-circle-outline'"></v-icon>
-            </v-scale-transition>
+            <h3 class="title carousel-text">Selected {{ items[model].text }}</h3>
           </v-row>
-        </v-card>
-      </v-slide-item>
-    </v-slide-group>
-
-    <v-expand-transition>
-      <v-sheet v-if="model != null" color="grey lighten-4" height="200" tile>
-        <v-row class="fill-height" align="center" justify="center">
-          <h3 class="title carousel-text">Selected {{ items[model].text }}</h3>
-        </v-row>
-      </v-sheet>
-    </v-expand-transition>
+        </v-sheet>
+      </v-expand-transition>
+    </template>
+    <template v-else-if="loading">
+      <v-card>
+        <v-card-text class="headline font-weight-bold">Carregando... 😊</v-card-text>
+      </v-card>
+    </template>
+    <template v-else>
+      <v-card>
+        <v-card-text v-if="isDonation" class="headline font-weight-bold">Nenhuma ajuda foi encontrada 😢</v-card-text>
+        <v-card-text v-else class="headline font-weight-bold">Nenhum pedido ajuda foi encontrado 😢</v-card-text>
+      </v-card>
+    </template>
   </v-sheet>
 </template>
 
@@ -45,6 +59,8 @@ export default {
     title: String,
     icon: String,
     items: {},
+    loading: Boolean,
+    isDonation: Boolean
   },
   data: () => ({
     model: null,
--- src/components/Home/Index.vue
+++ src/components/Home/Index.vue
@@ -1,19 +1,25 @@
 <template>
   <v-container fluid>
-    <v-row v-if="donations.items.length > 0" align="center" justify="center">
+    <v-row align="center" justify="center">
       <v-col cols="12">
         <donations-carousel
-        :title="donations.title"
-        :items="donations.items"
-        :icon="donations.icon" />
+          :loading="loadingDonations"
+          :isDonation="true"
+          :title="donations.title"
+          :items="donations.items"
+          :icon="donations.icon"
+        />
       </v-col>
     </v-row>
-    <v-row v-if="needsHelp.items.length > 0" align="center" justify="center">
+    <v-row align="center" justify="center">
       <v-col cols="12">
         <donations-carousel
-        :title="needsHelp.title"
-        :items="needsHelp.items"
-        :icon="needsHelp.icon" />
+          :loading="loadingNeeds"
+          :isDonation="false"
+          :title="needsHelp.title"
+          :items="needsHelp.items"
+          :icon="needsHelp.icon"
+        />
       </v-col>
     </v-row>
   </v-container>
@@ -26,20 +32,22 @@ import config from "../../configs/api";
 export default {
   name: "home-index",
   components: {
-    DonationsCarousel
+    DonationsCarousel,
   },
   data() {
     return {
       donations: {
         title: "Para doação",
         icon: "fas fa-hand-holding-heart",
-        items: []
+        items: [],
       },
       needsHelp: {
         title: "Preciso de ajuda",
         icon: "fas fa-hands",
-        items: []
+        items: [],
       },
+      loadingDonations: false,
+      loadingNeeds: false,
     };
   },
   mounted() {
@@ -48,39 +56,39 @@ export default {
     this.getNeedsHelp();
   },
   methods: {
-
     async getDonations() {
+      this.loadingDonations = true;
       try {
-        let apiAddress = config.address + '/donations';
+        let apiAddress = config.address + "/donations";
 
-        const response = await fetch(
-          apiAddress
-        );
+        const response = await fetch(apiAddress);
 
         const data = await response.json();
 
         this.donations.items = data.items;
+        this.loadingDonations = false;
       } catch (error) {
+        this.loadingDonations = false;
         console.log(error);
       }
     },
 
     async getNeedsHelp() {
+      this.loadingNeeds = true;
       try {
-        let apiAddress = config.address + '/needs';
+        let apiAddress = config.address + "/needs";
 
-        const response = await fetch(
-          apiAddress
-        );
+        const response = await fetch(apiAddress);
 
         const data = await response.json();
 
         this.needsHelp.items = data.items;
+        this.loadingNeeds = false;
       } catch (error) {
         console.log(error);
+        this.loadingNeeds = false;
       }
     },
-
   },
 };
 </script>
